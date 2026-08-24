585c4d53e47d78f4436ba360717709b4e8eb03fb
Aditya Wanjari <69350192+aditya-wanjari25@users.noreply.github.com>
2026-08-24 15:27:31 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = []
+
+        def backtrack(index, subset):
+
+            if index == len(nums):
+                output.append(subset.copy())
+                return
+            
+            subset.append(nums[index])
+            backtrack(index+1, subset)
+
+            subset.pop()
+            backtrack(index+1, subset)
+        
+        backtrack(0,[])
+        return output
